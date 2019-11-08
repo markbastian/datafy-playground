5ff02e0c12424108ff71e00eb9a431ab91257569
Mark Bastian <mbastian@clearwateranalytics.com>
2019-11-08 10:38:40 -0700

    Working simple datafy/nav example

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/java.jdbc "0.7.8"]
+                 [seancorfield/next.jdbc "1.0.9"]
                  [com.h2database/h2 "1.4.197"]
                  [datascript "0.17.1"]])
---
+++ src/datafy_playground/datafy_jdbc_next.clj
@@ -0,0 +1,22 @@
+(ns datafy-playground.datafy-jdbc-next
+  (:require [next.jdbc :as jdbc]
+            [next.jdbc.sql :as sql]
+            [next.jdbc.result-set :as rs]
+            [clojure.datafy :refer :all]))
+
+(def people
+  [{:id 1 :name "Richard Parker" :spouseid 2}
+   {:id 2 :name "Mary Parker" :spouseid 1}])
+
+(comment
+  (with-open [c (jdbc/get-connection {:dbtype "h2" :dbname "mem:test_mem1"})]
+    (jdbc/execute! c ["DROP TABLE PERSON"])
+    (jdbc/execute! c ["CREATE TABLE person (id int not null primary key, name varchar(32), spouseid int)"])
+    (let [k (keys (first people))]
+      (sql/insert-multi! c :person k (map #(map % k) people)))
+    (let [[res] (jdbc/execute! c ["SELECT * FROM PERSON WHERE name='Richard Parker'"])]
+      (-> res
+          (rs/datafiable-row c {:schema {:person/spouseid :person/id}})
+          datafy
+          (nav :person/spouseid 2))))
+  )
